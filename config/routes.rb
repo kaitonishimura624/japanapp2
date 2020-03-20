d3c202783303ab2910a389cd2f85de7a3c38528f
kaitonishimura624 <kaito.uw@gmail.com>
2020-03-21 01:29:58 +0900

    search-done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to:"posts#index"
+  namespace :posts do
+    resources :searches, only: :index
+  end
   resources :users, only: [:index, :show]
   resources :posts do
     resources :comments, only: [:create]
   end
-  root "posts#index"
 end
