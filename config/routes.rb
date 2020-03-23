52bcdaf1e919847e4a40811e5b688713cfe0e20e
kaitonishimura624 <kaito.uw@gmail.com>
2020-03-23 11:16:21 +0900

    likerankingdone

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   resources :posts do
     resources :comments, only: [:create]
-    resources :likes, only: [:create, :destroy]
+    resources :likes, only: [:index, :create, :destroy]
   end
 end
